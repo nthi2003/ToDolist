21c9e53501c40161097b84fdec147b388c7f71c1
nthi2003 <nthi20082003@gmail.com>
2023-03-02 20:45:57 +0700

    first commit

---
+++ src/Todos.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from "react";
+
+function Todos (){
+  return (
+<Fragment>
+    <p>Việc 1</p>
+    <p>Việc 2</p>
+    <p>Việc 3</p>
+
+</Fragment>
+  )
+}
+export default Todos;
